d0177fc4d7a6843b2299e3fd142c900f8cd4de05
Tolga Pirim <tolgapirim25@gmail.com>
2022-09-13 11:20:00 +0300

    setup navigation

---
+++ app/src/main/java/com/prmto/googleauthapp/navigation/NavGraph.kt
@@ -0,0 +1,22 @@
+package com.prmto.googleauthapp.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+
+@Composable
+fun SetupNavGraph(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Login.route
+    ) {
+        composable(route = Screen.Login.route){
+
+        }
+
+        composable(route = Screen.Profile.route){
+
+        }
+    }
+}
