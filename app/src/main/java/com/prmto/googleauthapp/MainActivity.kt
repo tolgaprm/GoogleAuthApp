e761bec2612fbf9d5efc3360ac35c9f5e76d7514
Tolga Pirim <tolgapirim25@gmail.com>
2022-09-13 11:38:29 +0300

    dagger-hilt setup

--- app/src/main/java/com/prmto/googleauthapp/MainActivity.kt
+++ app/src/main/java/com/prmto/googleauthapp/MainActivity.kt
@@ -13,7 +13,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import com.prmto.googleauthapp.navigation.SetupNavGraph
 import com.prmto.googleauthapp.ui.theme.GoogleAuthAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
